31a31c75391b691cf5114f0892c66a70001a84ce
saulo <saulo@192.168.15.11>
2018-06-09 01:47:41 -0300

    asdf

--- src/scrtc/aspose/RunTest.java
+++ src/scrtc/aspose/RunTest.java
@@ -1,10 +1,12 @@
 package scrtc.aspose;
 
+import com.aspose.pdf.Document;
+
 public class RunTest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
-		
+		Document d = new Document("original.pdf");
 		
 	}
 
